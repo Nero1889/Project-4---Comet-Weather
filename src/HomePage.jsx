146c9abb7f15a116d8320836f5d6fe90fd4910bc
Raul Luna <mceclipse1889@gmail.com>
2025-04-08 17:55:35 -0500

    Add files via upload

---
+++ src/HomePage.jsx
@@ -0,0 +1,14 @@
+function HomePage() {
+  return(<>
+        <h1>
+            <span className="blue">G</span>
+            <span className="red">o</span>
+            <span className="yellow">o</span>
+            <span className="blue">g</span>
+            <span className="green">l</span>
+            <span className="red">e</span>
+        </h1>
+  </>);
+}
+
+export default HomePage
